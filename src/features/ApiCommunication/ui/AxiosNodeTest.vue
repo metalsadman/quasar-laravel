524f4d16711025e6774db856c96a1a1245c34671
ren <renren@ixbase.net>
2019-11-15 11:58:45 +0800

    updates

--- src/features/ApiCommunication/ui/AxiosNodeTest.vue
+++ src/features/ApiCommunication/ui/AxiosNodeTest.vue
@@ -5,27 +5,17 @@
   >
     <div class="col-12 col-sm-6">
       <q-btn
-        label="test server"
+        label="Test Laravel API"
         @click="testServer"
         :loading="loading"
       />
-      <p class="text-h6">Test server response: {{ testResponse.resp }}</p>
-      <p class="text-caption">
-        If server error response, open this sandbox ->
-        <a
-          target="_blank"
-          href="https://codesandbox.io/s/b9g95"
-        >Test Node server</a>
-        to start the node server. You have to keep the tab open since the
-        sandbox server will shut down/hibernate after some time or when the
-        server page becomes inactive.
-      </p>
+      <p class="text-h6">Laravel Api Response: {{ testResponse }}</p>
     </div>
-    <div class="col-12 col-sm-6">
+    <div class="col-12 col-sm-6 q-gutter-md">
       <base-text-field
         v-model="url"
         label="Test URL"
-        hint="Enter endpoint for GET request"
+        hint="Enter endpoint for GET request. ie. https://codesandbox.io/s/b9g95"
         :val="$v.url"
         @input="$v.url.$touch"
         @blur="$v.url.$touch"
@@ -67,11 +57,11 @@ export default {
         .then(res => {
           this.loading = false
           this.testResponse = res.data.msg
-          this.notif = this.$q.notify({ color: 'positive', message: 'Server responded' })
+          // this.notif = this.$q.notify({ color: 'positive', message: 'Server responded' })
         })
         .catch(e => {
           this.loading = false
-          this.notif = this.$q.notify({ color: 'negative', message: e.message })
+          // this.notif = this.$q.notify({ color: 'negative', message: e.message })
         })
     },
     testUrl () {
